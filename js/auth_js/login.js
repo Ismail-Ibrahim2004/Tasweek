23d12d0335e2feecf76471ce28f35cc9f4728190
Ismail-Ibrahim2004 <esmealebrahim84@gmail.com>
2025-01-26 16:44:03 +0200

    add tasweek

---
+++ js/auth_js/login.js
@@ -0,0 +1,28 @@
+function validateForm(event) {
+  // إيقاف الإرسال الفوري للنموذج
+  event.preventDefault();
+
+  // الحصول على القيم المدخلة من المستخدم
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("pass").value.trim();
+
+  // التحقق من وجود قيم في الحقول
+  if ( !email || !password ) {
+      // إذا كان هناك حقل فارغ
+      document.getElementById("formMessage").innerHTML =
+          '<div class="error-message">Please complete all fields before submitting the form.</div>';
+      return false;
+  }
+
+  // إذا كانت جميع الحقول مملوءة
+  document.getElementById("formMessage").innerHTML =
+      '<div class="message">Success !</div>';
+
+  // تأخير لمدة نصف ثانية (500 مللي ثانية) قبل إرسال النموذج
+  setTimeout(function () {
+      // إرسال النموذج بعد نصف ثانية
+      document.getElementById("loginForm").submit();
+  }, 2000);
+
+  return false; // تأكد من أن النموذج لا يتم إرساله مباشرة
+}
